a0fbdd792c176182f680201e79509c2082dfeb28
Christian Colglazier <aquamorphcomputing@gmail.com>
2014-12-01 10:30:08 -0500

    Added top 5 scores

--- Lesson 9/Lab09.cpp
+++ Lesson 9/Lab09.cpp
@@ -5,11 +5,12 @@
 // Programmer: Christian Colglazier  
 //
 // Description:
-//    Bank management program
+//    Grade management program
 //                                                                  
 ///////////////////////////////////////////////////////////////////////
 
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <sstream>
 #include <string>
@@ -28,40 +29,27 @@ struct node
 	node *next;
 };
 
-void menu();
-void subMenu();
+
+
 void add(node *&head, node *&last);
-int genAccount();
-void addCustomer(node *&head, node *&last, string, string, double, double, int);
-void processCustomer(node *current, int, int);
+char getGrade(double);
+void addStudent(node *&head, node *&last, string, string, double, char);
 void printAll(node *current);
 void loadUsers(node *&head, node *&last);
 bool isEmpty(node *head);
-void addFirst(node *&head, node *&last, string, string, double, double, int);
+void addFirst(node *&head, node *&last, string, string, double, char);
 void remove(node *&head, node *&last);
+void highest(node *current);
 
-//Prints menu
-void menu()
-{
-	cout << "1: Enter 1 to add a new customer." << endl;
-	cout << "2: Enter 2 for an existing customer." << endl;
-	cout << "3: Enter 3 to print customers data." << endl;
-	cout << "9: Enter 9 to exit the program." << endl;
-}
 
-//Prints sub menu
-void subMenu()
+//Returns letter grade of score
+char getGrade(double score)
 {
-	cout << "1: Enter 1 to make a deposit." << endl;
-	cout << "2: Enter 2 to withdraw." << endl;
-	cout << "3: Enter 3 to check balance." << endl;
-	cout << "9: Enter 9 to exit." << endl;
-}
-
-//Creates a random account number
-int genAccount()
-{
-	return rand() % 9000 + 1000;
+	if(score>=90)return 'A';
+	else if(score>=80)return 'B';
+	else if(score>=70)return 'C';
+	else if(score>=60)return 'D';
+	else return 'F';
 }
 
 //Checks to see if the node is already created
@@ -72,59 +60,36 @@ bool isEmpty(node *head)
 }
 
 //Sets up the node and adds first entry
-void addFirst(node *&head, node *&last, string name, string type, double amount, double rate, int account)
+void addFirst(node *&head, node *&last, string lastName, string firstName, double score, char grade)
 {
 	node *temp = new node;
-	temp->name = name;
-	temp->type = type;
-	temp->amount = amount;
-	temp->rate = rate;
-	temp->account = account;
+	temp->lastName = lastName;
+	temp->firstName = firstName;
+	temp->score = score;
+	temp->grade = grade;
 	temp->next = NULL;
 	head = temp;
 	last = temp;
 }
 
-//Adds a customer to the node
-void addCustomer(node *&head, node *&last, string name, string type, double amount, double rate, int account)
+//Adds a student to the node
+void addStudent(node *&head, node *&last, string lastName, string firstName, double score, char grade)
 {
-	if(isEmpty(head))addFirst(head, last, name, type, amount, rate, account);
+	if(isEmpty(head))addFirst(head, last, lastName, firstName, score, grade);
 	else
 	{
 		node *temp = new node;
-		temp->name = name;
-		temp->type = type;
-		temp->amount = amount;
-		temp->rate = rate;
-		temp->account = account;
+		temp->lastName = lastName;
+		temp->firstName = firstName;
+		temp->score = score;
+		temp->grade = grade;
 		temp->next = NULL;
 		last->next = temp;
 		last = temp;
 	}
 }
 
-//Promts usere with a menu to add an account
-void add(node *&head, node *&last)
-{
-	string name, type;
-	double amount, rate;
-	int account = genAccount();
-	cin.clear();
-    cin.ignore(1000, '\n');
-	cout << "Enter customer's name: ";
-	getline(cin,name); 
-    cout << name << endl;
-	cout << "Enter account type (checking/savings): ";
-	cin >> type;
-	cout << "Enter amount to be deposited to open account: ";
-	cin >> amount;
-	cout << "Enter interest rate (as a percent): ";
-	cin >> rate;
-	cout << "Account number: " << account << endl;
-	addCustomer(head, last, name, type, amount, rate, account);
-}
-
-//Removes account
+//Removes student
 void remove(node *&head, node *&last)
 {
 	if(isEmpty(head));
@@ -141,100 +106,88 @@ void remove(node *&head, node *&last)
 	}
 }
 
-void processCustomer(node *current, int account, int choice)
-{
-	double amount;
-	if(choice==1)
-	{
-		while(current != NULL)
-		{
-			if(current->account==account)
-			{
-				cout << "Enter deposite amount: ";
-				cin >> amount;
-				current->amount = current->amount + amount;
-			}
-			current = current->next;
-		}
-	}
-	if(choice==2)
-	{
-		while(current != NULL)
-		{
-			if(current->account==account)
-			{
-				cout << "Enter withdraw amount: ";
-				cin >> amount;
-				current->amount = current->amount - amount;
-			}
-			current = current->next;
-		}
-	}
-	if(choice==3)
-	{
-		while(current != NULL)
-		{
-			if(current->account==account)
-			{
-				cout << "Account Holder Name: " << current->name << endl;
-				cout << "Account Type: " << current->type << endl;
-				cout << "Account Number: " << current->account << endl;
-				cout << "Balance: " << current->amount << endl;
-				cout << "Interest Rate: " << current->rate << endl;
-				cout << "*****************************" << endl;
-				cout << endl;
-			}
-			current = current->next;
-		}		
-	}
-	
-}
 
 //Loads accounts from a text file
 void loadUsers(node *&head, node *&last)
 {
-	string line,first,lastN,name,type;
-	double amount,rate;
-	int account;
+	string line,lastName,firstName;
+	double score;
+	char grade;
 	ifstream infile;
-	infile.open("customers.txt");
+	infile.open("Ch9_Ex2Data.txt");
 	
 	//checks for file opening
 	if (!infile)
 	{
 		cout << "Cannot open the input file. Program terminates!" << endl;
-		menuChoice=9;
 	}
 	
 	while(infile.good())
 	{
 		getline(infile, line);
 		stringstream stream(line);
-		stream >> first;
-		stream >> lastN;
-		name=first+" "+lastN;
-		stream >> type;
-		stream >> amount;
-		stream >> rate;
-		account = genAccount();
-		addCustomer(head, last, name, type, amount, rate, account);
+		stream >> firstName;
+		stream >> lastName;
+		stream >> score;
+		grade = getGrade(score);
+		addStudent(head, last, lastName, firstName, score, grade);
 		line.clear();
 	}
 }
 
-//Displays all user data
+//Displays all data
 void printAll(node *current)
 {
+	cout << setw(16) << left << "Student Name" << setw(11) << left << "Test Score" << "Grade" << endl;
 	while(current != NULL)
 	{
-		cout << "Account Holder Name: " << current->name << endl;
-		cout << "Account Type: " << current->type << endl;
-		cout << "Account Number: " << current->account << endl;
-		cout << "Balance: " << current->amount << endl;
-		cout << "Interest Rate: " << current->rate << endl;
-		cout << "*****************************" << endl;
-		cout << endl;
+		string name = current->lastName + ", " + current->firstName;
+		cout << setw(20) << left << name << setw(9) << left <<current->score << current->grade << endl;
 		current = current->next;
+	}
+}
+
+//Displays all data
+void highest(node *current)
+{
+	double high[5];
+	string names[5];
+	
+	while(current != NULL)
+	{
+		if(current->score>high[0])
+		{
+			high[0]=current->score;
+			names[0]=current->lastName + ", " + current->firstName;
+		}
+		else if(current->score>high[1])
+		{
+			high[1]=current->score;
+			names[1]=current->lastName + ", " + current->firstName;
+		}
+		else if(current->score>high[2])
+		{
+			high[2]=current->score;
+			names[2]=current->lastName + ", " + current->firstName;
+		}
+		else if(current->score>high[3])
+		{
+			high[3]=current->score;
+			names[3]=current->lastName + ", " + current->firstName;
+		}
+		else if(current->score>high[4])
+		{
+			high[4]=current->score;
+			names[4]=current->lastName + ", " + current->firstName;
+		}
+		current = current->next;
+	}
+	
+	cout << "Highest Test Score: " << high[0] << endl;
+	cout << "Students having the highest test score:" << endl;
+	for(int i=0;i<5;i++)
+	{
+		cout << names[i] << endl;
 	}
 }
 
@@ -243,32 +196,8 @@ int main()
 	node *head = NULL;
 	node *last = NULL;
 	loadUsers(head, last);
-	int account;
-	while(menuChoice!=9)
-	{
-		menu();
-		cin >> menuChoice;
-		if(menuChoice==1)
-		{
-			add(head,last);
-		}
-		if(menuChoice==2)
-		{
-			cout << "Enter account number: ";
-			cin >> account;
-			subMenuChoice=0;
-			while(subMenuChoice!=9)
-			{
-				subMenu();
-				cin >> subMenuChoice;
-				processCustomer(head,account,subMenuChoice);
-			}
-			
-		}
-		if(menuChoice==3)
-		{
-			printAll(head);
-		}
-	}
+	printAll(head);
+	cout << endl;
+	highest(head);
 	return 0;
 }
